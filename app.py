ca4d6ed71414d1afeac4a3807d555abc5a092497
timquiros Macbook <timquiros@timquirosMac.local>
2021-01-05 16:13:57 +0800

    edit new variables

--- app.py
+++ app.py
@@ -19,19 +19,20 @@ from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precisio
 from sklearn.metrics import precision_score, recall_score
 
 def prepare_model_input(feats):
-    loanptrate = np.log(feats[0] + 1)
-    loan_type = labels_dict['loantype'][feats[1]]
-    me_lack_req = labels_dict['me_lack_req'][feats[2]]
-    loan_amount = np.log(feats[3] + 1)
-    me_svc_stat = labels_dict['me_svc_stat'][feats[4]]
-    gross_pay = np.log(feats[5] + 1)
-    pnp_bill_mode = labels_dict['pnpbillmode'][feats[6]]
+
+    loanptrate = np.log(feats['term'] + 1)
+    loan_type = labels_dict['loantype'][feats['type']]
+    age = np.log(feats['age'] + 1)
+    loan_amount = np.log(feats['amount'] + 1)
+    me_svc_stat = labels_dict['me_svc_stat'][feats['svc_stat']]
+    gross_pay = np.log(feats['gross_pay'] + 1)
+    pnp_bill_mode = labels_dict['pnpbillmode'][feats['bill_mode']]
 
     return np.array(
         [
             loanptrate,
             loan_type,
-            me_lack_req,
+            age,
             loan_amount,
             me_svc_stat,
             gross_pay,
@@ -64,8 +65,7 @@ def plot_roc_auc(y_test, y_pred_proba):
 def load_data():
     x_test = pd.read_csv(
         os.path.join(os.getcwd(), 'datasets/x_test.csv'),
-        usecols=['loanptrate', 'loantype', 'me_lack_req', 'loanamt', 'me_svc_stat',
-       'grosspay1', 'pnpbillmode', 'outsloanamt']
+        usecols=['loanptrate','loantype','me_age','me_svc_stat','loanamt','grosspay1','pnpbillmode']
     )
     y_test = pd.read_csv(
         os.path.join(os.getcwd(), 'datasets/y_test.csv'),
@@ -80,16 +80,11 @@ def main():
     st.sidebar.title("Client Information Form")
     st.sidebar.markdown("Please fill up the all the required information below to calculate the credit score of the client.")
     
-    model = joblib.load(os.path.join(os.getcwd(), 'decision_tree.joblib'))
     # x_train, x_test, y_train, y_test = split(df)
 
-    #Drop down for Loan Type
-    st.sidebar.subheader("What loan type is the member applying for?")
-    loan_type = st.sidebar.selectbox("Loan_Type", [i for i in labels_dict['loantype'].keys()])
-
-    #Loan Term input bar
-    st.sidebar.subheader("How long in months is the client applying for?")
-    loanptrate = st.sidebar.slider("Loan Term", 1, 60, step=1, key='term')
+    #Age
+    st.sidebar.subheader("How old is the client?")
+    age = st.sidebar.number_input("Age", 18, 120, step=1, key='age')
 
     #Drop down for PNP Bill Mode
     st.sidebar.subheader("Choose the appropriate PNP bill mode")
@@ -99,27 +94,35 @@ def main():
     st.sidebar.subheader("What's the member's current service status?")
     me_svc_stat = st.sidebar.selectbox("Service Status", [i for i in labels_dict['me_svc_stat'].keys()])
 
-    #Drop down for Lack of Requeirements
-    st.sidebar.subheader("Does the member still have pending requirements in his/her application?")
-    me_lack_req = st.sidebar.radio("Lacking Requirements", [i for i in labels_dict['me_lack_req'].keys()], key='lack_req')
+    #Drop down for Loan Type
+    st.sidebar.subheader("What loan type is the member applying for?")
+    loan_type = st.sidebar.selectbox("Loan_Type", [i for i in labels_dict['loantype'].keys()])
+
+    #Loan Term input bar
+    st.sidebar.subheader("How long in months is the client applying for?")
+    loanptrate = st.sidebar.slider("Loan Term", 1, 60, step=1, key='term')
 
     #Loan Amount input bar
     st.sidebar.subheader("How much is the client applying for (in PHP)?")
-    loan_amount = st.sidebar.number_input("Loan Amount", 5000.0, 10000000.0, step=1000.0, key='loanamt')
+    loan_amount = st.sidebar.number_input("Loan Amount", 5000.0, 10000000.0, step=1000.0, key='loan_amount')
 
     #Gross Pay input bar
     st.sidebar.subheader("How much is the client's gross pay (in PHP)?")
     gross_pay = st.sidebar.number_input("Gross Pay", 0.0, 10000000.0, step=1000.0, key='gross_pay')
 
-    X = prepare_model_input([
-        loanptrate,
-        loan_type,
-        me_lack_req,
-        loan_amount,
-        me_svc_stat,
-        gross_pay,
-        pnp_bill_mode
-    ])
+    feat_input = {
+        'term': loanptrate,
+        'type': loan_type,
+        'age': age,
+        'svc_stat': me_svc_stat,
+        'amount': loan_amount,
+        'gross_pay': gross_pay,
+        'bill_mode': pnp_bill_mode
+    }
+
+    X = prepare_model_input(feat_input)
+    model = joblib.load(os.path.join(os.getcwd(), 'decision_tree.joblib'))
+    st.write(X)
 
     if st.sidebar.button("Calculate Credit Score", key='credit_score_button'):
         output_text = "The clien'ts probabilty to default the loan is "
--- labels.py
+++ labels.py
@@ -15,10 +15,6 @@ labels_dict = {
       "PL":12,
       "SL":13
    },
-   "me_lack_req":{
-      "No":0,
-      "Yes":1
-   },
    "me_svc_stat":{
       "AC":0,
       "CO":1,
@@ -39,5 +35,13 @@ labels_dict = {
       "PBM05":6,
       "PBM06":7,
       "PBM07":8
-   },
+   }
 }
+
+features = ['Term', 'Loan type', 'me_age', 'loanamt', 'me_svc_stat',
+       'grosspay1', 'pnpbillmode', 'me_app_place', 'me_mem_pay_type',
+       'unit_code', 'place_cd', 'me_rank', 'me_member_stat', 'me_cap_con',
+       'branch_code', 'me_sex', 'me_brn_srv', 'me_lack_req',
+       'me_svc_class', 'loanappl', 'outsloanamt', 'paymode',
+       'me_member_type', 'me_civil', 'me_hcity_town', 'nlp2',
+       'lneffintrate', 'lnintratepa']
